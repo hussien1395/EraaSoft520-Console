93a60d5ee94b90abf64ec680cd4f5024f5d09785
hussienyousif <hussien@oasisoft.net>
2025-07-23 18:20:47 +0300

    Add project files.

---
+++ EraaSoft520-Console/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EraaSoft520_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // (1) Challenge
+            Console.WriteLine("EraaSoft520");
+
+            // (2) Challenge
+            Console.WriteLine("Write your fav number from 1 to 100");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wow you number " + number + " is my fav also");
+            Console.WriteLine($"Wow you number {number} is my fav also");
+            Console.WriteLine("Wow you number {0} is my fav also", number);
+        }
+    }
+}
