8908f4683c601f6eb965f92084173449b5e99779
hussienyousif <hussien@oasisoft.net>
2025-08-06 12:42:52 +0300

    -

---
+++ Session 006 Challenges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_006_Challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
