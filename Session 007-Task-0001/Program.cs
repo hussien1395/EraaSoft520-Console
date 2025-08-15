083965df7cbe54716857cb92391aac0e5fb3cdf0
hussienyousif <hussien@oasisoft.net>
2025-08-15 21:08:37 +0300

    -

--- Session 007-Task-0001/Program.cs
+++ Session 007-Task-0001/Program.cs
@@ -1,54 +1,92 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Session_007_Task_0001
 {
     internal class Program
     {
-        
+        public static List<Instructor> allInstructors = new List<Instructor>();
+        public static List<Course> allCourses = new List<Course>();
+        public static List<Student> allStudents = new List<Student>();
+
         static void Main(string[] args)
         {
-            List<Instructor> allInstructors = new List<Instructor>();
-            List<Course> allCourses = new List<Course>();
-            List<Student> allStudents = new List<Student>();
 
-            // Instructors
-            Instructor instructor1 = AddInstructor();
-            allInstructors.Add(instructor1);
+            int option;
 
-            Instructor instructor2 = AddInstructor();
-            allInstructors.Add(instructor2);
+            do
+            {
+                PrintMenu();
+                option = SelectOption();
 
-            // Courses
-            Course course1 = AddCourse(instructor1);
-            allCourses.Add((Course)course1);
+                switch (option)
+                {
+                    case 1:
+                        AddStudent(new List<Course>());
+                        break;
+                    case 2:
+                        AddInstructor();
+                        break;
+                    case 3:
+                        AddCourse(allInstructors);
+                        break;
+                    case 4:
+                        EnrollStudentInCourse(allStudents, allCourses);
+                        break;
+                    case 5:
+                        DisplayAllStudent(allStudents);
+                        break;
+                    case 6:
+                        DisplayAllCourse(allCourses);
+                        break;
+                    case 7:
+                        DisplayAllInstructor(allInstructors);
+                        break;
+                    case 8:
+                        var std= SearchStudentById(allStudents);
+                        DisplayStudent(std);
+                        break;
+                    case 9:
+                        var course= SearchCourseById(allCourses);
+                        DisplayCourse(course);
+                        break;
+                    case 10:
+                        DeleteStudent(allStudents);
+                        break;
+                    case 11:
+                        UpdateStudent(allStudents, allCourses);
+                        break;
+                    case 12:
+                        break;
+                    default:
+                        Console.WriteLine("Unknown selection, please try again");
+                        Console.WriteLine("<<<---------------------------------------->>>");
+                        break;
+                }
+            } while (option != 12);
+        }
 
-            Course course2 = AddCourse(instructor1);
-            allCourses.Add((Course)course2);
+        static int SelectOption()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
 
-            // Students
-            Student student1 = AddStudent(new List<Course>());
-            allStudents.Add(student1);
-
-            Student student2 = AddStudent(new List<Course>());
-            allStudents.Add(student2);
-
-            EnrollStudentInCourse(allStudents, allCourses);
-            DisplayAllInstructor(allInstructors);
-            DisplayAllCourse(allCourses);
-            DisplayAllStudent(allStudents);
-
-            Console.WriteLine("Enter student ID to Search");
-            int stdID = Convert.ToInt32(Console.ReadLine());
-            var student= SearchStudentById(allStudents, stdID);
-            DisplayStudent(student);
-
-            UpdateStudent(allStudents, allCourses);
-            DisplayAllStudent(allStudents);
-
-            DeleteStudent(allStudents);
-            DisplayAllStudent(allStudents);
-
+        public static void PrintMenu()
+        {
+            Console.WriteLine(" 1.Add Student");
+            Console.WriteLine(" 2.Add Instructor");
+            Console.WriteLine(" 3.Add Course");
+            Console.WriteLine(" 4.Enroll Student in Course");
+            Console.WriteLine(" 5.Show All Students");
+            Console.WriteLine(" 6.Show All Courses");
+            Console.WriteLine(" 7.Show All Instructors");
+            Console.WriteLine(" 8.Find the student by id or name");
+            Console.WriteLine(" 9.Fine the course by id or name");
+            Console.WriteLine(" 10.Delete Student");
+            Console.WriteLine(" 11.Update Student");
+            Console.WriteLine(" 12.Exit");
         }
 
         public static Instructor AddInstructor()
@@ -68,10 +106,11 @@ namespace Session_007_Task_0001
                 Specialization = InstructorSpecialization
             };
 
+            allInstructors.Add(instructor);
             return instructor;
         }
 
-        public static Course AddCourse(Instructor instructor)
+        public static Course AddCourse(List<Instructor> instructors)
         {
             Console.WriteLine("Add Course");
             Console.WriteLine("Enter course id:");
@@ -79,12 +118,23 @@ namespace Session_007_Task_0001
             Console.WriteLine("Enter course title:");
             string CourseTitle = Console.ReadLine();
 
+            Console.WriteLine("Select Instructor for this course:");
+            for (int i = 0; i < instructors.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {instructors[i].Name} (ID: {instructors[i].InstructorId})");
+            }
+
+            int instructorChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+            Instructor selectedInstructor = instructors[instructorChoice];
+
             Course course = new Course
             {
                 CourseId = CourseId,
                 Title = CourseTitle,
-                Instructor = instructor 
+                Instructor = selectedInstructor
             };
+
+            allCourses.Add(course);
             return course;
         }
 
@@ -106,6 +156,7 @@ namespace Session_007_Task_0001
                 Courses = courses
             };
 
+            allStudents.Add(student);
             return student;
         }
 
@@ -147,6 +198,14 @@ namespace Session_007_Task_0001
             }
         }
 
+        public static void DisplayCourse(Course course)
+        {
+            Console.WriteLine("<<< Course >>>");
+            Console.WriteLine($"Course ID: {course.CourseId}");
+            Console.WriteLine($"Title: {course.Title}");
+            Console.WriteLine($"Instructor Name: {course.Instructor.Name}");
+        }
+
         public static void DisplayStudent(Student student)
         {
             Console.WriteLine("<<< Student >>>");
@@ -160,22 +219,35 @@ namespace Session_007_Task_0001
             }
         }
 
-        public static Student SearchStudentById(List<Student> students, int id)
+        public static Student SearchStudentById(List<Student> students)
         {
+            Console.WriteLine("Enter student ID to Search");
+            int stdID = Convert.ToInt32(Console.ReadLine());
+
             for (int i = 0; i < students.Count; i++)
             {
-                if (students[i].StudentId == id)
+                if (students[i].StudentId == stdID)
                     return students[i];
+            }
+            return null;
+        }
+
+        public static Course SearchCourseById(List<Course> courses)
+        {
+            Console.WriteLine("Enter course ID to Search");
+            int courseID = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < courses.Count; i++)
+            {
+                if (courses[i].CourseId == courseID)
+                    return courses[i];
             }
             return null;
         }
 
         public static bool UpdateStudent(List<Student> students, List<Course> allCourses)
         {
-            Console.WriteLine("Please enter student ID to update information");
-            int stdID = Convert.ToInt32(Console.ReadLine());
-
-            var student = SearchStudentById(students, stdID);
+            var student = SearchStudentById(students);
             if (student == null)
             {
                 Console.WriteLine("Student not found.");
@@ -214,10 +286,7 @@ namespace Session_007_Task_0001
 
         public static bool DeleteStudent(List<Student> students)
         {
-            Console.WriteLine("Please enter student ID to delete:");
-            int stdID = Convert.ToInt32(Console.ReadLine());
-
-            var student = SearchStudentById(students, stdID);
+            var student = SearchStudentById(students);
             if (student == null)
             {
                 Console.WriteLine("Student not found.");
---
+++ Session 008 Challenges 002/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Session_008_Challenges_002
+{
+
+    class Generic<T>
+    {
+        public static void PrintTwice(T x)
+        {
+            Console.WriteLine($"{x} {x}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            double y = 5.5;
+            string name = "Hussien";
+            Generic<int>.PrintTwice(x);
+            Generic<string>.PrintTwice(name);
+            Generic<double>.PrintTwice(y);
+
+        }
+    }
+}
